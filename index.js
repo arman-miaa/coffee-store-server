2d0a705f9d1a39744d936a2306973f02bfb3a996
Arman <mdarman365506@gmail.com>
2024-11-26 06:31:49 +0600

    added env option

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
@@ -11,10 +12,10 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7argw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // const uri =
-//   "mongodb+srv://<db_username>:<db_password>@cluster0.7argw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+//   "mongodb+srv://coffeeMaster:LM4L56pYZNXQW0YX@cluster0.7argw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -36,7 +37,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
